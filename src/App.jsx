a4ce809ca7631adb31a74909c4eefeae639a15de
Abhishek Sharma <112703149+xRaksh@users.noreply.github.com>
2023-02-11 18:28:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
 
       <BrowserRouter>
         <Nav />
-        <Home/>
         <Routes>
           <Route  path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
